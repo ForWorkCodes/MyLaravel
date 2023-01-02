2cbae2dd38e81a8f578161b7e333b8c344107fbd
ForWorkCodes <ForWorkCodes@gmail.com>
2023-01-02 14:50:24 +0300

    Стандартная структура наполнения

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\Post::factory(10)->create([
+            'thumbnail' => "public/storage/posts/1.jpg"
+        ]);
+        \App\Models\AdminUser::factory(1)->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.ru',
+            'password' => bcrypt('123456')
+        ]);
     }
 }
